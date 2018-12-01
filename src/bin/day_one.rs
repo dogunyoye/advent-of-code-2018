469225f5a84eaf60d0f14803536a50fb926c4d86
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2018-12-01 18:18:12 +0000

    Add minor usage doc for day one

--- src/bin/day_one.rs
+++ src/bin/day_one.rs
@@ -1,12 +1,16 @@
+//! `cargo run --bin day_one`
+
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 
 fn main() -> Result<()>{
     let file = File::open("src/bin/day_one_input.txt")?;
     let mut frequency = 0;
+
     for line in BufReader::new(file).lines() {
         frequency = frequency + line?.parse::<i32>().unwrap();
     }
+    
     println!("{}", frequency);
     Ok(())
 }
