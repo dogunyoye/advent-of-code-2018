5f5c7502eeeab3c80e4efa558b0486a54d2173a4
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2018-12-01 18:12:44 +0000

    Day One

---
+++ src/bin/day_one.rs
@@ -0,0 +1,12 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Result};
+
+fn main() -> Result<()>{
+    let file = File::open("src/bin/day_one_input.txt")?;
+    let mut frequency = 0;
+    for line in BufReader::new(file).lines() {
+        frequency = frequency + line?.parse::<i32>().unwrap();
+    }
+    println!("{}", frequency);
+    Ok(())
+}
