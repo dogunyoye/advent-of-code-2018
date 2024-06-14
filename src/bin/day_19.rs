ec777c0d7c2e492ce7b0e693735132aa93a4cd3d
Dimeji <dimejiogunyoye@googlemail.com>
2024-06-14 19:00:45 +0100

    Day 19 - part 2

--- src/bin/day_19.rs
+++ src/bin/day_19.rs
@@ -3,10 +3,39 @@
 mod day_16;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 
-fn main() -> Result<()>{
+fn run_process(instructions: &HashMap<String, fn(Vec<usize>, usize, usize, usize) -> Vec<usize>>, register: &mut Vec<usize>, mut program: Vec<String>, part_two: bool) {
+    let mut instruction_pointer: usize = 0;
+    let instruction_pointer_index: usize = program[0][4..5].parse::<usize>().unwrap();
+    let mut cycles = 0;
+
+    program.remove(0);
+
+    loop {
+
+        if instruction_pointer > program.len() - 1 {
+            break;
+        }
+
+        register[instruction_pointer_index] = instruction_pointer;
+
+        let operation: Vec<String> = program[instruction_pointer].split_whitespace()
+            .map(|x| x.to_string()).collect();
+
+        let operation_fn: fn(Vec<usize>, usize, usize, usize) -> Vec<usize> = *instructions.get(&operation[0].to_string()).unwrap();
+        *register = operation_fn(register.to_vec(), operation[1].parse::<usize>().unwrap(), operation[2].parse::<usize>().unwrap(), operation[3].parse::<usize>().unwrap()).to_vec();
+        instruction_pointer = register[instruction_pointer_index] + 1;
+        cycles += 1;
+
+        if part_two && cycles == 1000 {
+            return;
+        }
+    }
+}
+
+fn main() -> (){
 
     let mut instructions: HashMap<String, fn(Vec<usize>, usize, usize, usize) -> Vec<usize>> =
         HashMap::new();
@@ -28,33 +57,26 @@ fn main() -> Result<()>{
     instructions.insert("eqri".to_string(), day_16::eqri);
     instructions.insert("eqrr".to_string(), day_16::eqrr);
 
-    let mut lines: Vec<String> = BufReader::new(File::open("src/data/day_19_input.txt")?)
+    let program: Vec<String> = BufReader::new(File::open("src/data/day_19_input.txt").unwrap())
         .lines().map(|l| l.unwrap()).collect();
 
-    let mut register: Vec<usize> = vec![0, 0, 0, 0, 0, 0];
+    let mut registers_1: Vec<usize> = vec![0, 0, 0, 0, 0, 0];
+    run_process(&instructions, &mut registers_1, program.clone(), false);
+    let part_one = *registers_1.get(0).unwrap();
 
-    let mut instruction_pointer: usize = 0;
-    let instruction_pointer_index: usize = lines[0][4..5].parse::<usize>().unwrap();
+    println!("Part one: {}", part_one);
 
-    lines.remove(0);
+    // https://www.michaelfogleman.com/aoc18/#19
+    let mut registers_2: Vec<usize> = vec![1, 0, 0, 0, 0, 0];
+    run_process(&instructions, &mut registers_2, program.clone(), true);
+    let mut part_two = 0;
+    let n = registers_2.into_iter().max().unwrap();
 
-    loop {
-
-        if instruction_pointer > lines.len() - 1 {
-            break;
+    for i in 1..n + 1 {
+        if n % i == 0 {
+            part_two += i;
         }
-
-        register[instruction_pointer_index] = instruction_pointer;
-
-        let operation: Vec<String> = lines[instruction_pointer].split_whitespace()
-            .map(|x| x.to_string()).collect();
-
-        let operation_fn: fn(Vec<usize>, usize, usize, usize) -> Vec<usize> = *instructions.get(&operation[0].to_string()).unwrap();
-        register = operation_fn(register, operation[1].parse::<usize>().unwrap(), operation[2].parse::<usize>().unwrap(), operation[3].parse::<usize>().unwrap());
-        instruction_pointer = register[instruction_pointer_index];
-        instruction_pointer += 1;
     }
 
-    println!("Part one: {}", register[0]);
-    Ok(())
+    println!("Part two: {}", part_two);
 }
