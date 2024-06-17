f28af2e804210eca22a84dd552db5803af4d53cb
Dimeji <dimejiogunyoye@googlemail.com>
2024-06-17 01:47:26 +0100

    Day 20 - part 1

--- src/bin/day_20.rs
+++ src/bin/day_20.rs
@@ -1,6 +1,6 @@
 //! `cargo run --bin day_20`
 
-use std::{collections::HashMap, fs::File, io::{BufRead, BufReader}};
+use std::{cmp, collections::{HashMap, HashSet}, fs::File, io::{BufRead, BufReader}};
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 struct Position {
@@ -56,45 +56,83 @@ fn move_position(dir: char, pos: &mut Position, map: &mut HashMap<Position, char
     }
 }
 
-fn explore(regex: &String, mut pos: &mut Position, pointer: &mut usize, map: &mut HashMap<Position, char>) {
-    let mut c: char;
+fn dfs(map: &HashMap<Position, char>) -> usize {
+    let start: Position = Position{i: 0, j: 0};
+    let steps: usize = 0;
+    let mut visited: HashSet<Position> = HashSet::new();
+    let mut stack: Vec<(Position, usize)> = Vec::new();
+    let mut max_steps = usize::MIN;
 
-    while *pointer != regex.len() - 1 {
-        c = regex.chars().nth(*pointer).unwrap();
+    stack.push((start, steps));
 
-        if c == '(' {
-            *pointer += 1;
-            explore(regex, &mut pos.clone(), pointer, map);
-            c = regex.chars().nth(*pointer).unwrap();
+    while stack.len() != 0 {
+        let (current_position, current_steps) = stack.pop().unwrap();
+
+        if !visited.contains(&current_position) {
+            visited.insert(current_position);
+            max_steps = cmp::max(max_steps, current_steps);
+
+            let neighbours = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+            for n in neighbours {
+                let mut next_pos = Position{i: current_position.i + n.0, j: current_position.j + n.1};
+                let v = map.get(&next_pos);
+                if v.is_none() {
+                    continue;
+                }
+    
+                if *v.unwrap() == '|' || *v.unwrap() == '-' {
+                    next_pos.i = current_position.i + (2 * n.0);
+                    next_pos.j = current_position.j + (2 * n.1);
+    
+                    if *map.get(&next_pos).unwrap() != '.' {
+                        panic!("Found door without open position at: {:?}", next_pos);
+                    }
+
+                    stack.push((next_pos, current_steps + 1));
+                }
+            }
         }
-
-        if c == '|' || c == ')' {
-            *pointer += 1;
-            return;
-        }
-
-        move_position(c, &mut pos, map);
-        *pointer += 1;
     }
+
+    return max_steps;
 }
 
-fn find_largest_number_of_door_to_reach_a_room() -> i32 {
-    let mut regex: String = "".to_string();
+fn explore_iterative(regex: &String) -> HashMap<Position, char> {
     let mut map: HashMap<Position, char> = HashMap::new();
-    let mut pointer: usize = 1;
     let mut position = Position{i:0, j:0};
+    let mut stack: Vec<Position> = Vec::new();
 
-    map.insert(position, 'X');
+    for c in regex.chars() {
+        match c {
+            '(' => {
+                stack.push(position.clone());
+            },
+            '|' => {
+                position = stack.get(stack.len() - 1).unwrap().clone();
+            },
+            ')' => {
+                position = stack.pop().unwrap();
+            },
+            _ => {
+                move_position(c, &mut position, &mut map);
+            }
+        }
+    }
+
+    map.insert(Position{i: 0, j: 0}, '.');
+    return map;
+}
+
+fn find_largest_number_of_door_to_reach_a_room() -> usize {
+    let mut regex: String = "".to_string();
 
     for line in BufReader::new(File::open("src/data/day_20_input.txt").unwrap()).lines() {
         regex = line.unwrap();
+        regex = regex[1..regex.len()-1].to_string();
     }
 
-    println!("{}", regex);
-    explore(&regex, &mut position, &mut pointer, &mut map);
-    print_map(&map);
-
-    return 0;
+    let map: HashMap<Position, char> = explore_iterative(&regex);
+    return dfs(&map);
 }
 
 fn main() -> (){
