b1699516caa97ebfdc3f536dbfe51cfc236df3ea
Dimeji Ogunyoye <dimeji.ogunyoye@diffusiondata.com>
2024-06-20 21:39:19 +0100

    Day 23 - start of octree implementation

--- src/bin/day_23.rs
+++ src/bin/day_23.rs
@@ -11,8 +11,71 @@ struct Nanobot {
     r: usize
 }
 
+#[derive(Debug, Clone)]
+struct Cube {
+    min_x: f32,
+    max_x: f32,
+    min_y: f32,
+    max_y: f32,
+    min_z: f32,
+    max_z: f32,
+    corners: Vec<(f32, f32, f32)>
+}
+
+impl Cube {
+    fn split(&mut self) -> Vec<Cube> {
+        let middle_x = (self.min_x + self.max_x)/2.0;
+        let middle_y = (self.min_y + self.max_y)/2.0;
+        let middle_z = (self.min_z + self.max_z)/2.0;
+        
+        let c0 = build_cube(vec![self.min_x, middle_x, self.min_y, middle_y, self.min_z, middle_z]);
+        let c1 = build_cube(vec![self.min_x, middle_x, middle_y, self.max_y, self.min_z, middle_z]);
+        let c2 = build_cube(vec![self.min_x, middle_x, self.min_y, middle_y, middle_z, self.max_z]);
+        let c3 = build_cube(vec![self.min_x, middle_x, middle_y, self.max_y, middle_z, self.max_z]);
+        let c4 = build_cube(vec![middle_x, self.max_x, self.min_y, middle_y, self.min_z, middle_z]);
+        let c5 = build_cube(vec![middle_x, self.max_x, middle_y, self.max_y, self.min_z, middle_z]);
+        let c6 = build_cube(vec![middle_x, self.max_x, self.min_y, middle_y, middle_z, self.max_z]);
+        let c7 = build_cube(vec![middle_x, self.max_x, middle_y, self.max_y, middle_z, self.max_z]);
+
+        return vec![c0, c1, c2, c3, c4, c5, c6, c7];
+    }
+}
+
 fn manhattan_distance(first: &Nanobot, second: &Nanobot) -> i32 {
     return (first.x - second.x as i32).abs() + (first.y - second.y as i32).abs() + (first.z - second.z as i32).abs();
+}
+
+fn build_cube(bounds: Vec<f32>) -> Cube {
+    let min_x = bounds[0];
+    let max_x = bounds[1];
+    let min_y = bounds[2];
+    let max_y = bounds[3];
+    let min_z = bounds[4];
+    let max_z = bounds[5];
+
+    let mut corners: Vec<(f32, f32, f32)> = Vec::new();
+    for x in vec![min_x, max_x] {
+        for y in vec![min_y, max_y] {
+            for z in vec![min_z, max_z] {
+                corners.push((x, y, z));
+            }
+        }
+    }
+
+    return Cube{min_x, max_x, min_y, max_y, min_z, max_z, corners};
+
+}
+
+fn build_bounding_box(nanobots: Vec<Nanobot>) -> Cube {
+    let mut bounds: Vec<f32> = Vec::new();
+    bounds.push(nanobots.iter().map(|n| n.x).min().unwrap() as f32);
+    bounds.push(nanobots.iter().map(|n| n.x).max().unwrap() as f32);
+    bounds.push(nanobots.iter().map(|n| n.y).min().unwrap() as f32);
+    bounds.push(nanobots.iter().map(|n| n.y).max().unwrap() as f32);
+    bounds.push(nanobots.iter().map(|n| n.z).min().unwrap() as f32);
+    bounds.push(nanobots.iter().map(|n| n.z).max().unwrap() as f32);
+
+    return build_cube(bounds);
 }
 
 fn build_nanobots() -> (Vec<Nanobot>, Nanobot) {
@@ -60,6 +123,9 @@ fn calculate_nanobots_in_range_of_largest() -> i32 {
 }
 
 fn find_shortest_manhattan_distance_between_point_in_range_of_most_nanobots() -> i32 {
+    let (nanobots, _) = build_nanobots();
+    let bounding_box = build_bounding_box(nanobots);
+    println!("cube: {:?}", bounding_box);
     return 0;
 }
 
