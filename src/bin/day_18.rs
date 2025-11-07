9d855db4afd328aa15dd0789c324a951fc5e02cb
Dimeji <dimejiogunyoye@googlemail.com>
2025-11-07 22:03:28 +0000

    Day 18 - refactor and optimise part 2

--- src/bin/day_18.rs
+++ src/bin/day_18.rs
@@ -1,5 +1,6 @@
 //! `cargo run --bin day_18`
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -12,6 +13,23 @@ struct Acre {
 struct Point {
     x: i32,
     y: i32
+}
+
+fn grid_to_string(grid: &Vec<Vec<Acre>>) -> String {
+    return grid
+        .iter()
+        .flatten()
+        .map(|x|
+            if x.current == '#' {
+                return "#"
+            } else if x.current == '|' {
+                return "|"
+            } else {
+                return "?"
+            }
+        )
+        .collect::<Vec<_>>()
+        .join(" ");
 }
 
 fn build_initial_grid() -> Vec<Vec<Acre>> {
@@ -33,232 +51,139 @@ fn build_initial_grid() -> Vec<Vec<Acre>> {
     grid
 }
 
-fn part1() -> () {
+fn simulate(grid: &mut Vec<Vec<Acre>>, grid_size: usize) {
+    for x in 0..grid_size {
+        let x_coord = x as i32;
 
-    let grid_size = 50;
-    let mut grid = build_initial_grid();
-    let mut minutes = 10;
+        for y in 0..grid_size {
+            let current = grid[x][y].current;
+            let mut adjacent_points: Vec<Point> = Vec::new();
+            let y_coord = y as i32;
 
-    while minutes != 0 {
-        for x in 0..grid_size {
-            let x_coord = x as i32;
+            for dx in [-1, 0, 1] {
+                for dy in [-1, 0, 1] {
+                    if dx == 0 && dy == 0 {
+                        continue;
+                    }
+                    adjacent_points.push(Point { x: x_coord + dx, y: y_coord + dy })
+                }
+            }
 
-            for y in 0..grid_size {
-                let current = grid[x][y].current;
-                let mut adjacent_points: Vec<Point> = Vec::new();
-                let y_coord = y as i32;
+            let mut tree_count = 0;
+            let mut lumberyard_count = 0;
 
-                let top_left = Point { x: x_coord - 1, y: y_coord - 1 };
-                let top = Point { x: x_coord - 1, y: y_coord };
-                let top_right = Point { x: x_coord - 1, y: y_coord + 1 };
-                let left = Point { x: x_coord, y: y_coord - 1 };
-                let right = Point { x: x_coord, y: y_coord + 1 };
-                let bottom_left = Point { x: x_coord + 1, y: y_coord - 1 };
-                let bottom = Point { x: x_coord + 1, y: y_coord };
-                let bottom_right = Point { x: x_coord + 1, y: y_coord + 1 };
-
-                adjacent_points.push(top_left);
-                adjacent_points.push(top);
-                adjacent_points.push(top_right);
-                adjacent_points.push(left);
-                adjacent_points.push(right);
-                adjacent_points.push(bottom_left);
-                adjacent_points.push(bottom);
-                adjacent_points.push(bottom_right);
-
-                let mut tree_count = 0;
-                let mut lumberyard_count = 0;
-
-                for p in adjacent_points {
-                    if p.x >= 0 && p.x < grid_size as i32 && p.y >= 0 && p.y < grid_size as i32 {
-                        let acre: &Acre = &grid[p.x as usize][p.y as usize];
-                        if acre.current == '|' {
-                            tree_count += 1;
-                        }
-                        else if acre.current == '#' {
-                            lumberyard_count += 1;
-                        }
+            for p in adjacent_points {
+                if p.x >= 0 && p.x < grid_size as i32 && p.y >= 0 && p.y < grid_size as i32 {
+                    let acre: &Acre = &grid[p.x as usize][p.y as usize];
+                    match acre.current {
+                        '|' => {
+                            tree_count += 1
+                        },
+                        '#' => {
+                            lumberyard_count += 1
+                        },
+                        _ => {}
                     }
                 }
+            }
 
-                match current {
-                    '.' => {
-                        if tree_count >= 3 {
-                            grid[x][y].next = '|';
-                        }
-                        else {
-                            grid[x][y].next = current;
-                        }
-                    },
-                    '|' => {
-                        if lumberyard_count >= 3 {
-                            grid[x][y].next = '#';
-                        }
-                        else {
-                            grid[x][y].next = current;
-                        }
-                    },
-                    '#' => {
-                        if lumberyard_count >= 1 && tree_count >= 1 {
-                            grid[x][y].next = current;
-                        }
-                        else {
-                            grid[x][y].next = '.'
-                        }
-                    },
-                    _ => {}
-                }
+            match current {
+                '.' => {
+                    if tree_count >= 3 {
+                        grid[x][y].next = '|';
+                    }
+                    else {
+                        grid[x][y].next = current;
+                    }
+                },
+                '|' => {
+                    if lumberyard_count >= 3 {
+                        grid[x][y].next = '#';
+                    }
+                    else {
+                        grid[x][y].next = current;
+                    }
+                },
+                '#' => {
+                    if lumberyard_count >= 1 && tree_count >= 1 {
+                        grid[x][y].next = current;
+                    }
+                    else {
+                        grid[x][y].next = '.'
+                    }
+                },
+                _ => {}
             }
         }
-
-        for x in 0..grid_size {
-            for y in 0..grid_size {
-                grid[x][y].current = grid[x][y].next;
-            }
-        }
-
-        minutes -= 1;
     }
 
+    for x in 0..grid_size {
+        for y in 0..grid_size {
+            grid[x][y].current = grid[x][y].next;
+        }
+    }
+}
+
+fn calculate_total(grid: &Vec<Vec<Acre>>, grid_size: usize) -> usize {
     let mut lumberyard_count = 0;
     let mut wooded_acre_count = 0;
 
     for x in 0..grid_size {
         for y in 0..grid_size {
-            if grid[x][y].current == '#' {
-                lumberyard_count += 1;
-            }
-            else if grid[x][y].current == '|' {
-                wooded_acre_count += 1
+            match grid[x][y].current {
+                '#' => {
+                    lumberyard_count += 1
+                },
+                '|' => {
+                    wooded_acre_count += 1
+                },
+                _ => {}
             }
         }
     }
 
-    println!("Part 1: {}", lumberyard_count * wooded_acre_count);
+    lumberyard_count * wooded_acre_count
+}
+
+fn part1() -> () {
+    let grid_size = 50;
+    let mut grid = build_initial_grid();
+    let mut minutes = 10;
+
+    while minutes != 0 {
+        simulate(&mut grid, grid_size);
+        minutes -= 1;
+    }
+
+    println!("Part 1: {}", calculate_total(&grid, grid_size));
 }
 
 fn part2() -> () {
-
     let grid_size = 50;
     let mut grid = build_initial_grid();
     let mut minutes = 1000000000;
 
-    let mut len = 0;
-    let mut sequence_started = false;
-    let mut sequence_vec: Vec<i32> = Vec::new();
+    let mut sequence_vec: Vec<usize> = Vec::new();
+    let mut seen: HashMap<String, i32> = HashMap::new();
 
     while minutes != 0 {
-        for x in 0..grid_size {
-
-            let x_coord = x as i32;
-            for y in 0..grid_size {
-                let current = grid[x][y].current;
-                let mut adjacent_points: Vec<Point> = Vec::new();
-                let y_coord = y as i32;
-
-                let top_left = Point { x: x_coord - 1, y: y_coord - 1 };
-                let top = Point { x: x_coord - 1, y: y_coord };
-                let top_right = Point { x: x_coord - 1, y: y_coord + 1 };
-                let left = Point { x: x_coord, y: y_coord - 1 };
-                let right = Point { x: x_coord, y: y_coord + 1 };
-                let bottom_left = Point { x: x_coord + 1, y: y_coord - 1 };
-                let bottom = Point { x: x_coord + 1, y: y_coord };
-                let bottom_right = Point { x: x_coord + 1, y: y_coord + 1 };
-
-                adjacent_points.push(top_left);
-                adjacent_points.push(top);
-                adjacent_points.push(top_right);
-                adjacent_points.push(left);
-                adjacent_points.push(right);
-                adjacent_points.push(bottom_left);
-                adjacent_points.push(bottom);
-                adjacent_points.push(bottom_right);
-
-                let mut tree_count = 0;
-                let mut lumberyard_count = 0;
-
-                for p in adjacent_points {
-                    if p.x >= 0 && p.x < grid_size as i32 && p.y >= 0 && p.y < grid_size as i32 {
-                        let acre: &Acre = &grid[p.x as usize][p.y as usize];
-                        if acre.current == '|' {
-                            tree_count += 1;
-                        }
-                        else if acre.current == '#' {
-                            lumberyard_count += 1;
-                        }
-                    }
-                }
-
-                match current {
-                    '.' => {
-                        if tree_count >= 3 {
-                            grid[x][y].next = '|';
-                        }
-                        else {
-                            grid[x][y].next = current;
-                        }
-                    },
-                    '|' => {
-                        if lumberyard_count >= 3 {
-                            grid[x][y].next = '#';
-                        }
-                        else {
-                            grid[x][y].next = current;
-                        }
-                    },
-                    '#' => {
-                        if lumberyard_count >= 1 && tree_count >= 1 {
-                            grid[x][y].next = current;
-                        }
-                        else {
-                            grid[x][y].next = '.'
-                        }
-                    },
-                    _ => {}
-                }
-            }
-        }
-
-        let mut lumberyard_count = 0;
-        let mut wooded_acre_count = 0;
-
-        for x in 0..grid_size {
-            for y in 0..grid_size {
-                grid[x][y].current = grid[x][y].next;
-                if grid[x][y].current == '#' {
-                    lumberyard_count += 1;
-                }
-                else if grid[x][y].current == '|' {
-                    wooded_acre_count += 1
-                }
-            }
-        }
-
-        let total = lumberyard_count * wooded_acre_count;
-
-        // Some "magic number" programming.. I noticed that there is
-        // a 28 number sequence after the 400th minute (iteration) by
-        // printing out totals to stdout. There is probably a general
-        // way of determining this
-        if total == 172765 {
-            if !sequence_started {
-                len = 1000000000 - minutes - 1;
-                sequence_started = true;
-            }
-            else {
-                break;
-            }
-        }
-
-        if sequence_started {
-            sequence_vec.push(total);
-        }
-
         minutes -= 1;
-    }
 
-    let index = ((1000000000 - len) % sequence_vec.len()) - 2;
-    println!("Part 2: {}", sequence_vec.get(index).unwrap());
+        simulate(&mut grid, grid_size);
+        let grid_string = grid_to_string(&grid);
+
+        if seen.contains_key(&grid_string) {
+            let cycle_start = seen.get(&grid_string).unwrap();
+            let cycle_length = (1000000000 - minutes) - cycle_start;
+            let remaining = (1000000000 - cycle_start) % cycle_length;
+            let result = sequence_vec.get((cycle_start + remaining - 1) as usize).unwrap();
+            println!("Part 2: {}", result);
+            return;
+        }
+
+        seen.insert(grid_string, 1000000000 - minutes);
+        sequence_vec.push(calculate_total(&grid, grid_size))
+    }
 }
 
 fn main() -> (){
